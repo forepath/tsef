20f9dd3587117dd43c300a806de75981b5dfcfb5
Marcel Menk <marcel.menk@forepath.io>
2026-09-04 13:15:20 +0200

    fix(shared): cookie consent settings button

--- libs/domains/shared/frontend/util-cookie-consent/src/lib/cookie-consent.config.ts
+++ libs/domains/shared/frontend/util-cookie-consent/src/lib/cookie-consent.config.ts
@@ -18,12 +18,16 @@ export const cookieConfig: NgcCookieConsentConfig = {
     },
   },
   type: 'opt-in',
+  animateRevokable: false,
+  revokeBtn:
+    '<div class="cc-revoke {{classes}}" role="button" tabindex="0" aria-label="{{policy}}"><i class="bi bi-cookie" aria-hidden="true"></i></div>',
   content: {
     message: $localize`:@@utilCookieConsent-message:This website uses cookies to ensure you get the best experience on our website.`,
     dismiss: $localize`:@@utilCookieConsent-dismiss:Got it!`,
     allow: $localize`:@@utilCookieConsent-allow:Accept`,
     deny: $localize`:@@utilCookieConsent-deny:Decline`,
     link: $localize`:@@utilCookieConsent-link:Learn more`,
+    policy: $localize`:@@utilCookieConsent-policy:Cookie Policy`,
     href: environment.cookieConsent.privacyPolicyUrl,
   },
 };
